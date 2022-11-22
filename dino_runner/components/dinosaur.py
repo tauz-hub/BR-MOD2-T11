6062e92c45acfc208cc51a757c26ad890fdfcf28
tauz-hub <tauasanto101@gmail.com>
2022-11-22 00:01:43 -0300

    TAREFA AULA 01: Adicionando Função Abaixar

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,5 +1,5 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
@@ -15,6 +15,7 @@ class Dinosaur:
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = JUMP_VEL
 
     def run(self):
@@ -30,12 +31,20 @@ class Dinosaur:
         elif self.dino_jump:
             self.jump()
 
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump and not self.dino_duck or user_input[pygame.K_SPACE] and not self.dino_jump and not self.dino_duck:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+
+        if user_input[pygame.K_DOWN]:
+            self.jump_vel = JUMP_VEL
+            self.dino_jump = False
+            self.duck()
+
+        else:
+            self.dino_duck = False
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -52,7 +61,12 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        pass
+        self.dino_duck = True
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = 80
+        self.dino_rect.y = 345
+        self.step_index += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
